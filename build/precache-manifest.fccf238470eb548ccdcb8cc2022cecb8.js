7d21930de1f9b5a735d1182a1e27ab63d51aa83e
bossxdev <suphanut_boss@outlook.com>
2020-12-14 00:36:14 +0700

    Create new deploy on firebase server.

---
+++ build/precache-manifest.fccf238470eb548ccdcb8cc2022cecb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda0edb55dda7ed3d9290a8ba67106cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33b408105feb3e6a57f6",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "33b408105feb3e6a57f6",
+    "url": "/static/js/2.b2461b92.chunk.js"
+  },
+  {
+    "revision": "a3d5da51d8489a02931a36aa8a33f4e5",
+    "url": "/static/js/2.b2461b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797b707cf445765875f2",
+    "url": "/static/js/main.2b5d0e60.chunk.js"
+  },
+  {
+    "revision": "9dd28d978d7e4dcbc41e",
+    "url": "/static/js/runtime-main.f28f1140.js"
+  }
+]);
