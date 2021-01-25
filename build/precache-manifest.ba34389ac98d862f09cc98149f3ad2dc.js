d6177e25be912d1dfbcf55e51138eafea4d2c887
bossxdev <suphanut_boss@outlook.com>
2021-01-25 15:42:14 +0700

    Update on 25.01.64 - อัพเดทโมเดล shopping bag (เสร็จแล้ว)

---
+++ build/precache-manifest.ba34389ac98d862f09cc98149f3ad2dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96f013f38f98c809f0cf14ee3248771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f69b175d3d8146d214db",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "27861af48151097eb64f",
+    "url": "/static/css/main.ce118faf.chunk.css"
+  },
+  {
+    "revision": "f69b175d3d8146d214db",
+    "url": "/static/js/2.d579260d.chunk.js"
+  },
+  {
+    "revision": "a3d5da51d8489a02931a36aa8a33f4e5",
+    "url": "/static/js/2.d579260d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27861af48151097eb64f",
+    "url": "/static/js/main.cfa265a1.chunk.js"
+  },
+  {
+    "revision": "9dd28d978d7e4dcbc41e",
+    "url": "/static/js/runtime-main.f28f1140.js"
+  }
+]);
