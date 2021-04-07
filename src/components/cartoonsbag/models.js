7d2b2a05ce86f645c4e565ded09162dd6d0b8bc6
bossxdev <suphanut_boss@outlook.com>
2021-04-07 09:23:26 +0700

    Update on 07.04.2564 - create models for cartoon bags.

---
+++ src/components/cartoonsbag/models.js
@@ -0,0 +1,19 @@
+import * as THREE from 'three';
+
+export const getPlaneASideShape = () => {};
+export const getPlaneATopShape = () => {};
+export const getPlaneASideD = () => {};
+export const getPlaneABottomSideShape = () => {};
+export const getPlaneABottomSideD = () => {};
+export const getPlaneBTop = () => {};
+export const getPlaneBSide = () => {};
+export const getPlaneBBottomShape = () => {};
+export const getPlaneBottomDShape = () => {};
+export const getPlaneBBottomLid = () => {};
+export const getPlaneGlueTop = () => {};
+export const getGlueLid = () => {};
+export const getGludLidD = () => {};
+export const getGlueCenterShape = () => {};
+export const getGlueCenterLidShape = () => {};
+export const getRopeFront = () => {};
+export const getRopeBack = () => {};
